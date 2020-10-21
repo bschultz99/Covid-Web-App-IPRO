4740a4ee87ea521a1fb41d75ca4ec2f8f6a069f9
schlutz <bryantschultz99@gmail.com>
2020-10-21 16:56:21 -0500

    That moment you find out it is a JSON file

--- WebScraperIllinois.py
+++ WebScraperIllinois.py
@@ -1,10 +1,12 @@
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
+import re
 import pprint
-
+import json
 #https://dph.illinois.gov/covid19/covid-19-testing-sites
 #Illinois government testing
 
-source = urllib.request.urlopen('https://dph.illinois.gov/covid19/covid-19-testing-sites').read()
-soup = BeautifulSoup(source, 'lxml')
-print(soup.body.nextsibling)
+url = "https://dph.illinois.gov/sitefiles/COVIDTestingSites.json"
+r = requests.get(url)
+cont = r.json()
+json.dumps(r, indent=4)
