579499bb07736ce23363963658191c6f441b06f5
D41Robot <dkeats12@gmail.com>
2020-10-22 14:47:32 -0500

    Update ZabeUpdater.sh

--- ZabeUpdater.sh
+++ ZabeUpdater.sh
@@ -1,11 +1,3 @@
 cd home/covid19webapp/Desktop/Covid-Wep-App-IPRO
 git pull
 cd ../../../../
-
-srcdir="home/covid19webapp/Desktop/Covid-Wep-App-IPRO"
-dstdir="var/www/html"
-
-for srcfile in ${srcdir}/*
-do
-    cp $srcfile $dstdir/$dstfile
-done
