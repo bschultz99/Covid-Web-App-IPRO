c33fa07c0964203307073db7df16b59c5f388914
D41Robot <dkeats12@gmail.com>
2020-10-22 15:52:46 -0500

    Update ZabeUpdater.sh
    Framework of what script will do.

--- ZabeUpdater.sh
+++ ZabeUpdater.sh
@@ -1,2 +1,13 @@
+date=$(date +"%m%d%y")
+
+#Location of all files
 cd /home/covid19webapp/Desktop/Covid-Wep-App-IPRO
+
+#Update ant changes of the website
 git pull
+
+#Run any necessary scripts
+
+#Commit info back to Github
+git commit -m "AutoCommit on $date"
+git push
