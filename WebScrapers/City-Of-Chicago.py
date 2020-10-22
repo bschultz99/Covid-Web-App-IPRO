6740d05f76b6fbdf95fae4e8dddd8871e8276bf9
schlutz <bryantschultz99@gmail.com>
2020-10-22 16:51:27 -0500

    Working on next scraper

---
+++ WebScrapers/City-Of-Chicago.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import csv
+
+#City of Chicago Data Portal
+#https://data.cityofchicago.org/Health-Human-Services/COVID-19-Testing-Sites/thdn-3grx/data
+
+url = "https://data.cityofchicago.org/resource/thdn-3grx.json"
+r = requests.get(url).json()
+data = []
+filename = "City-Of-Chicago.csv"
+fields = ['facility','phone','address','url']
+i = 0
+for site in r: 
+    data.append({
+        0 : site
+    })
+
+with open(filename, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    for testing in data:
+       for test in testing.values():
+           print(test['phone'])
+        #   row = [test['facility'], test['phone'], test['address'], test['web_site']['url']]
+         #  csvwriter.writerow(row)
