3dd9a5cc5a11ce224b941eea606b957e9a1019ce
schlutz <bryantschultz99@gmail.com>
2020-10-25 10:49:28 -0500

    Update City-Of-Chicago.sql

--- WebScrapers/City-Of-Chicago.sql
+++ WebScrapers/City-Of-Chicago.sql
@@ -1,6 +1,6 @@
 use testing_sites;
 
-TRUCNCATE chicago;
+TRUNCATE TABLE chicago;
 
 LOAD DATA LOCAL INFILE '/home/covid19webapp/Desktop/Covid-Wep-App-IPRO/WebScrapers/City-Of-Chicago.csv'
 INTO TABLE chicago
