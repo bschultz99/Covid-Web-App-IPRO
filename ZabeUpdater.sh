5d17131c8ae4f2271ba111997bc37c531662e3d1
D41Robot <dkeats12@gmail.com>
2020-10-25 16:34:58 -0500

    Add SQL script command

--- ZabeUpdater.sh
+++ ZabeUpdater.sh
@@ -6,9 +6,12 @@ cd /home/covid19webapp/Desktop/Covid-Wep-App-IPRO
 #Update ant changes of the website
 git pull
 
-#Run any necessary scripts
+#Run any python scripts
 python3 WebScrapers/DPH-Illinois.py
 python3 WebScrapers/City-Of-Chicago.py
+
+#Run any SQL scripts
+mysql --local-infile=1 -h localhost -u root -pZabeSaveMe2020! testing_sites < WebScraper.sql
 
 #Commit info back to Github
 git commit -m "AutoCommit on $date"
