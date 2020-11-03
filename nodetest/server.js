b58cedb234b7e999e63c489f8f315548904b6be2
Jared Wagler <jared.wagler@gmail.com>
2020-11-03 10:14:28 -0600

    Modified server.js to include sql connection

--- nodetest/server.js
+++ nodetest/server.js
@@ -1,8 +1,32 @@
+//Require statements
+var path = require('path');
+var mysql = require('mysql');
+
+//Exppress hosting
 var express = require('express');
 var app = express();
-var path = require('path');
-var requirejs = require('requirejs')
 
+//Create connection credentials
+console.log('starting mysql')
+var con = mysql.createConnection({
+    host: 'd41robot.asuscomm.com',
+    user: 'covid19webapp',
+    password: 'zabe'
+});
+
+//Start MySQL connection
+con.connect((err) => {
+    if(err){
+        console.log('Error connecting to Db');
+        return;
+    }
+    console.log('Connection established');
+});
+con.end((err) => {
+});
+
+//Points express to the correct folder
 app.use(express.static('./public'))
 
+//Listens on port 3000 for hosting
 app.listen(3000);
