83bb7a1ebbf2cfaece8905c87091925bd0a1ea15
Jared Wagler <jared.wagler@gmail.com>
2020-11-03 11:50:49 -0600

    Create map.js

---
+++ nodetest/map.js
@@ -0,0 +1,35 @@
+//Include server.js
+var mysql = require('mysql')
+
+//Set API Token
+mapboxgl.accessToken = 'pk.eyJ1IjoiandhZ2xlciIsImEiOiJja2d4dms3dGUwNWU0Mnl0OG5pcTZycTZlIn0.Cp002MjA3sIzuSSDoNWsYw';
+
+//Set the boundaries of the map
+var bounds = [
+	[-88.20, 41.60], //Southwest
+	[-87.39, 42.04] //Northeast
+]
+
+//Initialize the map
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [-87.64, 41.87],
+	zoom: 12.5,
+	maxBounds: bounds
+});
+
+//Create connection credentials
+console.log('starting mysql')
+var con = mysql.createConnection({
+    database: 'testing_sites',
+    host: 'localhost',
+    user: 'root',
+    password: 'ZabeSaveMe2020!'
+});
+
+//Start MySQL connection
+con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+});
