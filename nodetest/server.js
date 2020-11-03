c9ce8fb4b77073321a142075571163d906415436
Jared Wagler <jared.wagler@gmail.com>
2020-11-03 11:47:34 -0600

    Removed mysql from server.js

--- nodetest/server.js
+++ nodetest/server.js
@@ -1,28 +1,12 @@
 //Require statements
 var path = require('path');
-var mysql = require('mysql');
 
 //Exppress hosting
 var express = require('express');
 var app = express();
 
-//Create connection credentials
-console.log('starting mysql')
-var con = mysql.createConnection({
-    database: 'testing_sites',
-    host: 'localhost',
-    user: 'root',
-    password: 'ZabeSaveMe2020!'
-});
-
-//Start MySQL connection
-con.connect(function(err){
-    if (err) throw err;
-    console.log("Connected!");
-});
-
 //Points express to the correct folder
-app.use(express.static('./public'))
+app.use(express.static('./'))
 
 //Listens on port 3000 for hosting
 app.listen(3000);
