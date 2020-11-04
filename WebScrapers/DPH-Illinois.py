15e5a3814b42d29cbfe124719b29496e5597d985
schlutz <bryantschultz99@gmail.com>
2020-11-04 10:59:37 -0600

    Still working on GeoPy
    Also Jared is a giga gamer

--- WebScrapers/DPH-Illinois.py
+++ WebScrapers/DPH-Illinois.py
@@ -21,6 +21,8 @@ with open(filename, 'w', newline='') as csvfile:
     csvwriter.writerow(fields)
     for testing in data:
        for test in testing.values():
-           #print(geolocater.geocode(test['address'] + "," + test['city'] + "," + test['state'] + "," + test['zip']).latitude)
+           location = geolocater.geocode(test['address'] + "," + test['city'] + "," + test['state'] + "," + test['zip'])
+           if location is not None and location.longitude is not None:
+                print(location.longitude)
            row = [test['locationName'].replace(',', '*'), test['hours'].replace(',', '*'), test['daysofoperation'].replace(',', '*'), test['requirements'].replace(',', '*'), test['webcovidhotline'].replace(',', '*'), test['address'].replace(',', '*'), test['city'].replace(',', '*'), test['zip'].replace(',', '*'), test['website'].replace(',', '*'), test['state'].replace(',', '*'), test['languagesoffered'].replace(',', '*')]
            csvwriter.writerow(row)
